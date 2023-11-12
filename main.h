1bae4d63a87272da4b24d9e14e06a28559f77e55
AbdelazizElsawy <abdelaziz38elsawy@gmail.com>
2023-11-12 05:16:46 -0800

    adding print_int prototypes to the header files

--- main.h
+++ main.h
@@ -3,11 +3,24 @@
 
 #include <stdarg.h>
 #include <stddef.h>
+
+/**
+ * Abdelaziz
+ */
 int _printf(const char *format, ...);
 int _putchar(char c);
 int print_char(va_list val);
 int print_str(va_list val);
 int _strlen(char *s);
 int _strlens(const char *s);
+
+
+/**
+ * Aymane
+ */
+// i added these prototypes from print_int.c
+int print_d(va_list args);
+int print_i(va_list args);
+
 int print_dec(va_list args);
 #endif
