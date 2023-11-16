a30f781826ca2061b30eb8240c4b4ab6db7e4308
Abdelaziz Elsawy <abdelaziz38elsawy@gmail.com>
2023-11-16 23:33:13 +0200

    printf

--- print_hex.c
+++ print_hex.c
@@ -9,22 +9,22 @@
 
 int print_HEX(va_list ap, params_t *params)
 {
-        unsigned long l;
-        int c = 0;
-        char *str;
+	unsigned long l;
+	int c = 0;
+	char *str;
 
-        if (params->l_modifier)
-                l = (unsigned long)va_arg(ap, unsigned long);
-        else if (params->h_modifier)
-                l = (unsigned short int)va_arg(ap, unsigned int);
-        else
-                l = (unsigned int)va_arg(ap, unsigned int);
-        str = convert(1, 16, CONVERT_UNSIGNED, params);
-        if (params->hashtag_flag && l)
-        {
-                *--str = 'X';
-                *--str = '0';
-        }
-        params->unsign = 1;
-        return (c += print_number(str, params));
+	if (params->l_modifier)
+		l = (unsigned long)va_arg(ap, unsigned long);
+	else if (params->h_modifier)
+		l = (unsigned short int)va_arg(ap, unsigned int);
+	else
+		l = (unsigned int)va_arg(ap, unsigned int);
+	str = convert(1, 16, CONVERT_UNSIGNED, params);
+	if (params->hashtag_flag && l)
+	{
+		*--str = 'X';
+		*--str = '0';
+	}
+	params->unsign = 1;
+	return (c += print_number(str, params));
 }
