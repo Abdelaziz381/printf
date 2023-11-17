4d6e9d7980c6f9c757c2f7d7ec241a176893d7f5
Abdelaziz Elsawy <abdelaziz38elsawy@gmail.com>
2023-11-17 02:27:02 +0200

    a

--- main.h
+++ main.h
@@ -78,7 +78,7 @@ int print_char(va_list ap, params_t *params);
 int print_int(va_list ap, params_t *params);
 int print_str(va_list ap, params_t *params);
 int print_percent(va_list ap, params_t *params);
-int print_s(va_list ap, params_t *params);
+
 
 /* number func */
 
